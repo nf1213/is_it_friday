63ab39675bed0612ca50278265e550aaf2a7da02
Nicole <nfelch121394@gmail.com>
2014-12-10 13:44:05 -0500

    initial

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,15 @@
+class DaysController < ApplicationController
+
+  def index
+    @day = Day.find(Time.now.wday + 1)
+    @today_is_friday = Time.now.friday?
+  end
+
+  private
+
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.require(:days).permit(:name, :message)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Day.create(name: 'Sunday', message: 'Enjoy the end of your weekend!')
+Day.create(name: 'Monday', message: "It's only Monday... Good luck...")
+Day.create(name: 'Tuesday', message: "You've got a while to go...")
+Day.create(name: 'Wednesday', message: "It's humpday! Friday is on it's way!")
+Day.create(name: 'Thursday', message: "Tommorrow is Friday! YAY, almost there!")
+Day.create(name: 'Friday', message: "Hoorayyyy!!!")
+Day.create(name: 'Saturday', message: "Friday just passed, but now it's the weekend!! YAY!")
