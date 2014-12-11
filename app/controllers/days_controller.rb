c4a0e5c9de86433a57bb561ce735870bfde7b5a2
Nicole <nfelch121394@gmail.com>
2014-12-10 19:20:28 -0500

    Fixed Time again?

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,8 +1,8 @@
 class DaysController < ApplicationController
 
   def index
-    @day = Day.find(Time.localtime.wday + 1)
-    @today_is_friday = Time.localtime.friday?
+    @day = Day.find(Time.now.localtime.wday + 1)
+    @today_is_friday = Time.now.localtime.friday?
   end
 
   private
